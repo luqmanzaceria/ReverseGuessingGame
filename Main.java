a2f46278312e937a0261cc30ba3212ac7a8a1faa
luqmanzaceria <47729606+luqmanzaceria@users.noreply.github.com>
2019-04-06 21:15:40 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,92 @@
+
+import java.util.Scanner;
+import java.lang.String;
+import java.lang.Math;
+
+public class Main {
+    public static void main (String[] args) {
+
+        String choice;
+
+        System.out.println("Welcome to Reverse Guessing Game!");
+        System.out.println("Would you like to play?");
+
+
+        System.out.println("(Type yes or no)");
+        Scanner yesno = new Scanner(System.in);
+        choice = yesno.nextLine();
+
+        if (choice.equals("no")) {
+
+            System.out.println("Hope you play next time!");
+            System.exit(0);
+        }
+             else{
+
+
+            int maxnum;
+            int minnum = 1;
+
+
+            System.out.println("Please pick a range you would like to play in (1 - ?).");
+
+
+            //I need a code that will check if input is number
+
+
+            do {
+                System.out.println("The number should be greater than 2.");
+                Scanner range = new Scanner(System.in);
+
+                maxnum = range.nextInt();
+            } while (maxnum <= 2);
+
+
+            String proceed;
+
+            System.out.println("Your range is 1 - " + maxnum + ". Pick a number within your chosen range.");
+            int tries;
+            tries = 1;
+
+            do {
+                System.out.println("Enter (ready) when you have picked a number, or (quit) if you would like to exit the game.");
+
+                Scanner ready = new Scanner(System.in);
+                proceed = ready.nextLine();
+
+                if (proceed.equalsIgnoreCase("quit")) {
+                    System.out.println("Hope you play next time!");
+                    System.exit(0);
+
+                }
+            } while (!proceed.equalsIgnoreCase("ready"));
+            String conclusion;
+            do {
+                int guess;
+                guess = (Math.round(maxnum + minnum) / 2);
+
+                System.out.println("Is your number (higher), (lower), or the (same) as " + guess + "?");
+                Scanner rightorwrong = new Scanner(System.in);
+                conclusion = rightorwrong.nextLine();
+
+
+                if (conclusion.equalsIgnoreCase("higher")) {
+                    tries++;
+                    minnum = guess;
+
+                } else if (conclusion.equalsIgnoreCase("lower")) {
+                    tries++;
+                    maxnum = guess;
+                }
+            } while (!conclusion.equalsIgnoreCase("same"));
+            if (tries <= 1) {
+                System.out.println("The computer took " + tries + " try");
+                System.out.println("Thanks for playing!");
+
+            } else {
+                System.out.println("The computer took " + tries + " tries");
+                System.out.println("Thanks for playing!");
+            }
+        }
+    }
+}
